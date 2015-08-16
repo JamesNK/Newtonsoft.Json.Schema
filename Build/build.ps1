8d2b04a6e8a3e3e32187cfad6170d5c19c859f7f
James Newton-King <james@newtonking.com>
2015-08-16 20:24:30 +1200

    -Increment version to 1.0.10

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,7 +1,7 @@
 ﻿properties { 
   $zipFileName = "JsonSchema10r9.zip"
   $majorVersion = "1.0"
-  $majorWithReleaseVersion = "1.0.9"
+  $majorWithReleaseVersion = "1.0.10"
   $version = GetVersion $majorWithReleaseVersion
   $signAssemblies = $false
   $signKeyPath = "C:\Development\Releases\newtonsoft.snk"
