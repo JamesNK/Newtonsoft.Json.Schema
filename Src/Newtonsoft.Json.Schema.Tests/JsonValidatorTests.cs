c829404d0eee8b4456e6a4d4d7b9f15cbcb3787a
James Newton-King <james@newtonking.com>
2015-11-22 17:17:53 +1300

    -Tests

--- Src/Newtonsoft.Json.Schema.Tests/JsonValidatorTests.cs
+++ Src/Newtonsoft.Json.Schema.Tests/JsonValidatorTests.cs
@@ -75,11 +75,6 @@ namespace Newtonsoft.Json.Schema.Tests
             IList<string> errors;
             Assert.IsFalse(o.IsValid(schema, out errors));
 
-            foreach (string error in errors)
-            {
-                Console.WriteLine(error);
-            }
-
             Assert.AreEqual(2, errors.Count);
             Assert.AreEqual(@"Invalid type. Expected Object but got String. Path 'someDictionary[2].value', line 28, position 25.", errors[0]);
             Assert.AreEqual(@"Duplicate key: {""field1"":""value1"",""field2"":""value2"",""field3"":""value3""}. Path 'someDictionary', line 2, position 21.", errors[1]);
@@ -103,6 +98,78 @@ namespace Newtonsoft.Json.Schema.Tests
             public override bool CanValidate(JSchema schema)
             {
                 return schema.ExtensionData.ContainsKey("uniqueDictionaryKey");
+            }
+        }
+
+        [Test]
+        public void ValidateSimpleType()
+        {
+            string json = @"{
+  ""values"": [
+    ""1"",
+    ""[1]"",
+    ""\""A string!\"""",
+    123,
+    ""{\""prop1\"":bad!}""
+  ]
+}";
+
+            JSchemaReaderSettings settings = new JSchemaReaderSettings
+            {
+                Validators = new List<JsonValidator> { new JsonFormatValidator() }
+            };
+
+            JSchema schema = JSchema.Parse(@"{
+  ""type"": ""object"",
+  ""properties"": {
+    ""values"": {
+      ""type"": ""array"",
+      ""items"": {
+        ""type"": ""string"",
+        ""format"": ""json""
+      }
+    }
+  }
+}", settings);
+
+            JObject o = JObject.Parse(json);
+
+            IList<ValidationError> errors;
+            Assert.IsFalse(o.IsValid(schema, out errors));
+
+            Assert.AreEqual(2, errors.Count);
+
+            Assert.AreEqual(@"Invalid type. Expected String but got Integer.", errors[0].Message);
+            Assert.AreEqual(ErrorType.Type, errors[0].ErrorType);
+            Assert.AreEqual("#/properties/values/items/0", errors[0].SchemaId.OriginalString);
+
+            Assert.AreEqual(@"String is not JSON: Unexpected character encountered while parsing value: b. Path 'prop1', line 1, position 9.", errors[1].Message);
+            Assert.AreEqual(ErrorType.Validator, errors[1].ErrorType);
+            Assert.AreEqual("#/properties/values/items/0", errors[1].SchemaId.OriginalString);
+        }
+
+        public class JsonFormatValidator : JsonValidator
+        {
+            public override void Validate(JToken value, JsonValidatorContext context)
+            {
+                if (value.Type == JTokenType.String)
+                {
+                    string s = value.ToString();
+
+                    try
+                    {
+                        JToken.Parse(s);
+                    }
+                    catch (Exception ex)
+                    {
+                        context.RaiseError($"String is not JSON: {ex.Message}");
+                    }
+                }
+            }
+
+            public override bool CanValidate(JSchema schema)
+            {
+                return (schema.Format == "json");
             }
         }
     }
