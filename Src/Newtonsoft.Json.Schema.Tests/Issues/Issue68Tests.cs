be6a4021c25c9cfd620aa4d618c031f78458e457
James Newton-King <james@newtonking.com>
2016-12-23 14:22:03 +1300

    -Build?

--- Src/Newtonsoft.Json.Schema.Tests/Issues/Issue68Tests.cs
+++ Src/Newtonsoft.Json.Schema.Tests/Issues/Issue68Tests.cs
@@ -3,7 +3,7 @@
 // License: https://raw.github.com/JamesNK/Newtonsoft.Json.Schema/master/LICENSE.md
 #endregion
 
-#if !(NET20 || NET35 || PORTABLE) || DNXCORE50
+#if !(NET20 || NET35 || PORTABLE)
 using System;
 using System.Collections.Generic;
 using System.IO;
