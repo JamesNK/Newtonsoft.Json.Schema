0668f4a407073d0503357ca5e944432474465bb0
James Newton-King <james@newtonking.com>
2018-03-24 09:16:07 +1300

    -Increment version to 3.0.10

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,7 +1,7 @@
 ﻿properties { 
-  $zipFileName = "JsonSchema30r9.zip"
+  $zipFileName = "JsonSchema30r10.zip"
   $majorVersion = "3.0"
-  $majorWithReleaseVersion = "3.0.9"
+  $majorWithReleaseVersion = "3.0.10"
   $nugetPrerelease = $null
   $version = GetVersion $majorWithReleaseVersion
   $packageId = "Newtonsoft.Json.Schema"
