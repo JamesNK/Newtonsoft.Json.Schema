abb4321e372020a9a81e81b2cec043f174fba6f9
James Newton-King <james@newtonking.com>
2018-11-08 14:37:19 +1300

    Provide additional info for invalid IDs

---
+++ Src/Newtonsoft.Json.Schema.Tests/Issues/Issue0162Tests.cs
@@ -0,0 +1,47 @@
+﻿#region License
+// Copyright (c) Newtonsoft. All Rights Reserved.
+// License: https://raw.github.com/JamesNK/Newtonsoft.Json.Schema/master/LICENSE.md
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema.Infrastructure.Discovery;
+#if DNXCORE50
+using Xunit;
+using Test = Xunit.FactAttribute;
+using Assert = Newtonsoft.Json.Schema.Tests.XUnitAssert;
+#else
+using NUnit.Framework;
+#endif
+
+namespace Newtonsoft.Json.Schema.Tests.Issues
+{
+    [TestFixture]
+    public class Issue0162Tests : TestFixtureBase
+    {
+        [Test]
+        public void Test()
+        {
+            var settings = new JSchemaReaderSettings();
+            settings.ValidationEventHandler += OnValidate;
+
+            ExceptionAssert.Throws<JSchemaException>(
+                () => JSchema.Parse(Json, settings),
+                "Error resolving schema ID 'file:/action_scenario.json#' in the current scope. The resolved ID must be a valid URI. Path 'properties.prop1', line 3, position 14.");
+        }
+
+        private const string Json = @"{
+  ""properties"": {
+    ""prop1"": {
+      ""$id"": ""file:/action_scenario.json#""
+    }
+  }
+}";
+
+        private void OnValidate(object sender, SchemaValidationEventArgs e)
+        {
+        }
+    }
+}
