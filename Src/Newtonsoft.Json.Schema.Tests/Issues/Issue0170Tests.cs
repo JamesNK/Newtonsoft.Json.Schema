022e40beaaf64607b674951cf2115c8e780e16e1
James Newton-King <james@newtonking.com>
2019-02-22 11:06:30 +1300

    Fix validating property name with enum (#175)

---
+++ Src/Newtonsoft.Json.Schema.Tests/Issues/Issue0170Tests.cs
@@ -0,0 +1,68 @@
+﻿#region License
+// Copyright (c) Newtonsoft. All Rights Reserved.
+// License: https://raw.github.com/JamesNK/Newtonsoft.Json.Schema/master/LICENSE.md
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema.Infrastructure.Discovery;
+#if DNXCORE50
+using Xunit;
+using Test = Xunit.FactAttribute;
+using Assert = Newtonsoft.Json.Schema.Tests.XUnitAssert;
+#else
+using NUnit.Framework;
+#endif
+
+namespace Newtonsoft.Json.Schema.Tests.Issues
+{
+    [TestFixture]
+    public class Issue0170Tests : TestFixtureBase
+    {
+        [Test]
+        public void Test()
+        {
+            JSchema schema = JSchema.Parse(SchemaJson);
+
+            JArray a = JArray.Parse(Json);
+
+            bool result = a.IsValid(schema);
+
+            Assert.IsTrue(result);
+        }
+
+        private const string SchemaJson = @"{
+  ""type"": ""array"",
+  ""items"": {
+    ""oneOf"": [
+      {
+        ""type"": ""object"",
+        ""propertyNames"": {
+          ""type"": ""string"",
+          ""enum"": [
+            ""validName""
+          ]
+        },
+        ""additionalProperties"": true
+      },
+      {
+        ""type"": ""string"",
+        ""enum"": [
+          ""validName""
+        ]
+      }
+    ]
+  }
+}";
+
+        private const string Json = @"[
+  {
+    ""validName"": {
+      ""a"": 1
+    }
+  }
+]";
+    }
+}
