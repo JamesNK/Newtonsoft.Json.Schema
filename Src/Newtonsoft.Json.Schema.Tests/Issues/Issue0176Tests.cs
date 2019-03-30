ce2451b86e68c7548d0d35424f2c105ea8e97fba
Guillaume Girard <ggirard07@users.noreply.github.com>
2019-03-30 00:28:09 -0400

    Fix required property list not properly cached in ValidationError (#177)

---
+++ Src/Newtonsoft.Json.Schema.Tests/Issues/Issue0176Tests.cs
@@ -0,0 +1,69 @@
+﻿#region License
+// Copyright (c) Newtonsoft. All Rights Reserved.
+// License: https://raw.github.com/JamesNK/Newtonsoft.Json.Schema/master/LICENSE.md
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+#if DNXCORE50
+using Xunit;
+using Test = Xunit.FactAttribute;
+using Assert = Newtonsoft.Json.Schema.Tests.XUnitAssert;
+#else
+using NUnit.Framework;
+#endif
+
+namespace Newtonsoft.Json.Schema.Tests.Issues
+{
+    [TestFixture]
+    public class Issue0176Tests : TestFixtureBase
+    {
+        [Test]
+        public void Test()
+        {
+            // Arrange
+            JSchema schema = JSchema.Parse(SchemaJson);
+            JObject payload = JObject.Parse(Json);
+
+            // Act
+            bool result = payload.IsValid(schema, out IList<ValidationError> errors);
+
+            // Assert
+            Assert.AreEqual(new List<string> { "line1", "line2" }, errors[0].Value);
+        }
+
+        private const string SchemaJson = @"{
+            'type': 'object',
+            'properties': {
+                    'address': {
+                    'type': 'object',
+                    'properties': {
+                        'line1': {'type':'string'},
+                        'line2': {'type':'string'}
+                    },
+                    'required': [ 'line1', 'line2' ]
+                },
+                'address2': {
+                    'type': 'object',
+                    'properties': {
+                        'line1': {'type':'string'},
+                        'line2': {'type':'string'}
+                    },
+                    'required': [ 'line1', 'line2' ]
+                }
+            },
+            'required': [ 'address', 'address2' ]
+        }";
+
+        private const string Json = @"{
+            'address': {},
+            'address2': {
+                'line1': 'Bar',
+                'line2': 'Boo'
+            }
+        }";
+    }
+}
