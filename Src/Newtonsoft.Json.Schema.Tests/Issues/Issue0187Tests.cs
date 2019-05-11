6c05bea1544d9678fa7b5fb33dd66cb123003962
James Newton-King <james@newtonking.com>
2019-05-11 21:20:01 +1200

    Fix dependencies not being written (#188)

---
+++ Src/Newtonsoft.Json.Schema.Tests/Issues/Issue0187Tests.cs
@@ -0,0 +1,114 @@
+﻿#region License
+// Copyright (c) Newtonsoft. All Rights Reserved.
+// License: https://raw.github.com/JamesNK/Newtonsoft.Json.Schema/master/LICENSE.md
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+#if DNXCORE50
+using Xunit;
+using Test = Xunit.FactAttribute;
+using Assert = Newtonsoft.Json.Schema.Tests.XUnitAssert;
+#else
+using NUnit.Framework;
+#endif
+
+namespace Newtonsoft.Json.Schema.Tests.Issues
+{
+    [TestFixture]
+    public class Issue0187Tests : TestFixtureBase
+    {
+        [Test]
+        public void Test()
+        {
+            // Arrange
+            var schema = new JSchema()
+            {
+                Type = JSchemaType.Object,
+                SchemaVersion = new Uri("http://json-schema.org/draft-07/schema#")
+            };
+
+            schema.Properties.Add("ShowField", new JSchema() { Type = JSchemaType.Boolean });
+
+            var oneOf1 = new JSchema();
+            oneOf1.Properties.Add("ShowField", new JSchema() { Enum = { false } });
+
+            var oneOf2 = new JSchema();
+            oneOf2.Properties.Add("ShowField", new JSchema() { Enum = { true } });
+            oneOf2.Properties.Add("ExtraField", new JSchema() { Type = JSchemaType.String });
+
+            var oneOf = new JSchema();
+            oneOf.OneOf.Add(oneOf1);
+            oneOf.OneOf.Add(oneOf2);
+
+            schema.Dependencies.Add("ShowField", oneOf);
+
+            Console.WriteLine();
+
+            // Act
+            var result = schema.ToString();
+
+            // Assert
+            StringAssert.AreEqual(@"{
+  ""$schema"": ""http://json-schema.org/draft-07/schema#"",
+  ""type"": ""object"",
+  ""properties"": {
+    ""ShowField"": {
+      ""type"": ""boolean""
+    }
+  },
+  ""dependencies"": {
+    ""ShowField"": {
+      ""oneOf"": [
+        {
+          ""properties"": {
+            ""ShowField"": {
+              ""enum"": [
+                false
+              ]
+            }
+          }
+        },
+        {
+          ""properties"": {
+            ""ShowField"": {
+              ""enum"": [
+                true
+              ]
+            },
+            ""ExtraField"": {
+              ""type"": ""string""
+            }
+          }
+        }
+      ]
+    }
+  }
+}", result);
+        }
+
+        [Test]
+        public void RoundtripArray()
+        {
+            // Arrange
+            const string schemaJson = @"{
+  ""dependencies"": {
+    ""quux"": [
+      ""foo"",
+      ""bar""
+    ]
+  }
+}";
+
+            // Act
+            JSchema schema = JSchema.Parse(schemaJson);
+            string json = schema.ToString();
+
+            // Assert
+            Assert.AreEqual(schemaJson, json);
+        }
+    }
+}
