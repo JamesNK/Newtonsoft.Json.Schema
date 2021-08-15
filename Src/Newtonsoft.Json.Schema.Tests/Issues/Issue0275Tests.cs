e00fb27edd710a4e4270c08b607143d15bc67c84
James Newton-King <james@newtonking.com>
2021-08-15 16:13:54 +1200

    Fix divide by zero error with small multipleOf (#276)

---
+++ Src/Newtonsoft.Json.Schema.Tests/Issues/Issue0275Tests.cs
@@ -0,0 +1,96 @@
+﻿#region License
+// Copyright (c) Newtonsoft. All Rights Reserved.
+// License: https://raw.github.com/JamesNK/Newtonsoft.Json.Schema/master/LICENSE.md
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema.Generation;
+using Newtonsoft.Json.Serialization;
+#if DNXCORE50
+using Xunit;
+using Test = Xunit.FactAttribute;
+using Assert = Newtonsoft.Json.Schema.Tests.XUnitAssert;
+#else
+using NUnit.Framework;
+#endif
+
+namespace Newtonsoft.Json.Schema.Tests.Issues
+{
+    [TestFixture]
+    public class Issue0275Tests : TestFixtureBase
+    {
+        [Test]
+        public void Test()
+        {
+            JSchema schema = JSchema.Parse(Schema);
+            JObject o = JObject.Parse(Data);
+
+            Assert.IsTrue(o.IsValid(schema));
+        }
+
+        private const string Schema = @"{
+    ""$schema"": ""http://json-schema.org/draft-07/schema"",
+    ""type"": ""object"",
+    ""properties"": {
+        ""contentSource"": {
+            ""type"": ""string""
+        },
+        ""contentInterface"": {
+            ""type"": ""string""
+        },
+        ""contents"": {
+            ""type"": ""array"",
+            ""additionalItems"": false,
+            ""minItems"": 1,
+            ""items"": {
+                ""$ref"": ""#/definitions/contentItem""
+            }
+        }
+    },
+    ""definitions"": {
+        ""contentItem"": {
+            ""type"": ""object"",
+            ""properties"": {
+                ""dataObjectValue"": {
+                    ""$ref"": ""#/definitions/dataObjectValueType""
+                }
+            },
+            ""additionalProperties"": false
+        },
+        ""dataObjectValueType"": {
+            ""type"": ""object"",
+            ""properties"": {
+                ""lastOrderQTY"": {
+                    ""type"": ""number"",
+                    ""maximum"": 99999999999999999999.99,
+                    ""multipleOf"": 0.00000000000000000000000000001
+                }
+            }
+        }
+    },
+    ""additionalProperties"": false,
+    ""required"": [
+        ""contentSource"",
+        ""contentInterface"",
+        ""contents""
+    ]
+}";
+
+        private const string Data = @"{
+    ""contentSource"": ""Lorem"",
+    ""contentInterface"": ""Lorem"",
+    ""contents"": [
+        {
+            ""dataObjectValue"": {
+                ""lastOrderQTY"": 99999999999999999999.099
+            }
+        }
+    ]
+}";
+    }
+}
