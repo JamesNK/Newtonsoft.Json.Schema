660d5276156857289e0482b4501b54bde74cce36
Munir Husseini <mhusseini@softewareproduction.eu>
2021-09-16 09:08:50 +0200

    Added (On)SchemaGenerated to JSchemaGenerator
    The proposed addition is intended to allow users to easily manipulate the JSchema after is gets generated. This is especially helpful with inline schemas that are not reachable otherwise.

--- Src/Newtonsoft.Json.Schema/Generation/JSchemaGeneratorInternal.cs
+++ Src/Newtonsoft.Json.Schema/Generation/JSchemaGeneratorInternal.cs
@@ -309,6 +309,8 @@ namespace Newtonsoft.Json.Schema.Generation
                 PopulateSchema(schema, contract, memberProperty, resolvedRequired);
             }
 
+            _generator.OnSchemaGenerated(type, schema);
+
             return schema;
         }
 
